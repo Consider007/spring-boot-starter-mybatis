9e7a5dc21f0e96a6a10314427968c45ab6231f67
storezhang <storezhang@gmail.com>
2016-10-18 21:22:26 +0800

    1、增加配置；

--- src/main/java/com/ruijc/mybatis/MapperProperties.java
+++ src/main/java/com/ruijc/mybatis/MapperProperties.java
@@ -4,13 +4,34 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import tk.mybatis.spring.mapper.MapperScannerConfigurer;
 
-//@ConditionalOnClass(MapperScannerConfigurer.class)
+import javax.annotation.PostConstruct;
+
+@ConditionalOnClass(MapperScannerConfigurer.class)
 @ConfigurationProperties("mybatis.mapper")
 public class MapperProperties {
 
     private String mappers;
     private String notEmpty;
     private String identity;
+    private String catalog;
+    private String order;
+    private String schema;
+    private String seqFormat;
+    private String style;
+    private String enableMethodAnnotation;
+
+    @PostConstruct
+    public void init() {
+        mappers = "tk.mybatis.mapper.common.Mapper";
+        notEmpty = "true";
+        identity = "MYSQL";
+        catalog = "";
+        schema = "";
+        order = "AFTER";
+        seqFormat = "{0}.nextval";
+        style = "camelhump";
+        enableMethodAnnotation = "false";
+    }
 
     public String getNotEmpty() {
         return notEmpty;
@@ -34,5 +55,53 @@ public class MapperProperties {
 
     public void setIdentity(String identity) {
         this.identity = identity;
+    }
+
+    public String getCatalog() {
+        return catalog;
+    }
+
+    public void setCatalog(String catalog) {
+        this.catalog = catalog;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public String getSchema() {
+        return schema;
+    }
+
+    public void setSchema(String schema) {
+        this.schema = schema;
+    }
+
+    public String getSeqFormat() {
+        return seqFormat;
+    }
+
+    public void setSeqFormat(String seqFormat) {
+        this.seqFormat = seqFormat;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getEnableMethodAnnotation() {
+        return enableMethodAnnotation;
+    }
+
+    public void setEnableMethodAnnotation(String enableMethodAnnotation) {
+        this.enableMethodAnnotation = enableMethodAnnotation;
     }
 }
--- src/main/java/com/ruijc/mybatis/MybatisAutoConfiguration.java
+++ src/main/java/com/ruijc/mybatis/MybatisAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.ruijc.mybatis;
 
 import com.github.pagehelper.PageHelper;
+import com.ruijc.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -26,15 +27,21 @@ public class MybatisAutoConfiguration {
     private MapperProperties mapperProperties;
 
     @Bean
-    //@ConditionalOnClass(PageHelper.class)
+    @ConditionalOnClass(PageHelper.class)
     public PageHelper pageHelper(DataSource dataSource) {
         PageHelper pageHelper = new PageHelper();
 
         Properties properties = new Properties();
         if (null != helperProperties) {
+            properties.setProperty("dialect", helperProperties.getDialect());
             properties.setProperty("offsetAsPageNum", helperProperties.getOffsetAsPageNum());
             properties.setProperty("rowBoundsWithCount", helperProperties.getRowBoundsWithCount());
+            properties.setProperty("pageSizeZero", helperProperties.getPageSizeZero());
             properties.setProperty("reasonable", helperProperties.getReasonable());
+            properties.setProperty("params", helperProperties.getParams());
+            properties.setProperty("supportMethodsArguments", helperProperties.getSupportMethodsArguments());
+            properties.setProperty("returnPageInfo", helperProperties.getReturnPageInfo());
+            properties.setProperty("closeConn", helperProperties.getCloseConn());
         }
         pageHelper.setProperties(properties);
 
@@ -42,7 +49,7 @@ public class MybatisAutoConfiguration {
     }
 
     @Bean
-    //@ConditionalOnClass(MapperScannerConfigurer.class)
+    @ConditionalOnClass(MapperScannerConfigurer.class)
     public MapperScannerConfigurer mapperScannerConfigurer() {
         MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
 
@@ -53,6 +60,12 @@ public class MybatisAutoConfiguration {
             properties.setProperty("mappers", mapperProperties.getMappers());
             properties.setProperty("notEmpty", mapperProperties.getNotEmpty());
             properties.setProperty("IDENTITY", mapperProperties.getIdentity());
+            properties.setProperty("ORDER", mapperProperties.getOrder());
+            properties.setProperty("catalog", mapperProperties.getCatalog());
+            properties.setProperty("schema", mapperProperties.getSchema());
+            properties.setProperty("seqFormat", mapperProperties.getSeqFormat());
+            properties.setProperty("style", mapperProperties.getStyle());
+            properties.setProperty("enableMethodAnnotation", mapperProperties.getEnableMethodAnnotation());
         }
         mapperScannerConfigurer.setProperties(properties);
 
